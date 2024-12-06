05b0b9fb798f665c7977c5cf840fc133bab8edf6
Reza <rezakhonsari@icloud.com>
2024-12-07 00:23:24 +0330

    init commit

---
+++ UnguidedPlayer/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  UnguidedPlayer
+//
+// Created by Reza Khonsari on 12/7/24.
+// Copyright © 2024 Reza Khonsari. All rights reserved.
+//
+// Linkedin: https://www.linkedin.com/in/rezakhonsari-ios/
+// Github: https://github.com/rezamagnet
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnguidedPlayer/UnguidedPlayerApp.swift
@@ -0,0 +1,21 @@
+//
+//  UnguidedPlayerApp.swift
+//  UnguidedPlayer
+//
+// Created by Reza Khonsari on 12/7/24.
+// Copyright © 2024 Reza Khonsari. All rights reserved.
+//
+// Linkedin: https://www.linkedin.com/in/rezakhonsari-ios/
+// Github: https://github.com/rezamagnet
+//
+
+import SwiftUI
+
+@main
+struct UnguidedPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
